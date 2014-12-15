d4e8110bf0f07efa94de6c05495b5275b807682f
William Carter <william.carter@bbc.co.uk>
2014-12-15 19:13:51 +0000

    Added main js

--- require-config.js
+++ require-config.js
@@ -2,17 +2,45 @@ var package = require("./package.json");
 var coa = require("coa");
 
 module.exports = coa.Cmd()
-  .name("require-config")
-  .title("require-config")
-  .opt()
-    .name("version")
-    .title("Version")
-    .short("v")
-    .long("version")
-    .flag()
-    .act(function() {
-      return package.version;
+    .name("require-config")
+    .title("require-config")
+    .opt()
+        .name("version")
+        .title("Version")
+        .short("v")
+        .long("version")
+        .flag()
+        .act(function() {
+          return package.version;
+        })
+        .only()
+    .end()
+    .opt()
+        .name("mainFile")
+        .title("'main.js' file")
+        .short("m")
+        .long("mainfile")
+        .req()
+    .end()
+    .opt()
+        .name("input")
+        .title("input")
+        .short("i")
+        .long("input")
+        .arr()
+        .req()
+    .end()
+    .opt()
+        .name("output")
+        .title("output")
+        .short("o")
+        .long("output")
+    .end()
+    .act(function(options) {
+
+        // output file default
+        var output = options.output || "requirejs-configuration.js"
+
+        require("./src/main.js").main(options.mainFile, options.input, output);
     })
-    .only()
-  .end()
 .run();
---
+++ src/main.js
@@ -0,0 +1,10 @@
+function main(mainFile, input, output) {
+    console.log("require-config main()");
+    console.log(mainFile);
+    console.log(input);
+    console.log(output);
+}
+
+module.exports = {
+    main: main
+}
