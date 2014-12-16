224a68fb0da713ce692b42d42fe981e65c23b097
William Carter <william.carter@bbc.co.uk>
2014-12-16 23:19:15 +0000

    paths for phridge builder

--- src/phridge-config-builder.js
+++ src/phridge-config-builder.js
@@ -1,35 +1,124 @@
+require("es6-shim");
+var path = require("path");
 var phridge = require("phridge");
+var Utils = require("./utils.js");
+
 
 var requirejsPath = require.resolve("requirejs");
-var libraryPath = "/Users/cartew01/workspace/require-config/resources/amd.js";
-
-console.log(requirejsPath);
-console.log(libraryPath);
-
-phridge.spawn({
-    loadImages: false
-}).then(function (phantom) {
 
 
-    var page = phantom.createPage();
+function buildConfig(mainFile, inputFiles, completeCallback) {
+    console.log("phridge-config-builder - buildConfig()");
+    console.log(mainFile);
+    console.log(inputFiles);
 
-    page.run(libraryPath, function (libPath, resolve, reject) {
+    console.log(Utils);
+    var inputFilePaths = inputFiles.map(function(absolutePath) {
+        return path.resolve(absolutePath);
+    });
+    console.log(inputFilePaths);
 
-        var script = document.createElement('script');
-        script.onload = function() {
-            alert("Script loaded and ready");
-            requirejs([ libPath ], function(amd) {
-                resolve(amd);
-            });
+    var libraryPath = "/Users/cartew01/workspace/require-config/resources/amd.js";
 
-        };
-        script.src = "http://requirejs.org/docs/release/2.1.15/minified/require.js";
-        document.getElementsByTagName('head')[0].appendChild(script);
+    var config = {
+        paths: {},
+        shim:{}
+    };
 
-    }).then(function(returned) {
-        console.log("done");
-        console.log(returned);
-        phantom.dispose();
+
+    phridge.spawn({
+        loadImages: false
+    }).then(function (phantom) {
+        processInputFileIteration(inputFilePaths, phantom);
     });
 
-});
+    function processInputFileIteration(inputFilePaths, phantomProcess) {
+
+        console.log("processInputFileIteration");
+        console.log(inputFilePaths);
+
+        if (inputFilePaths.length < 1) {
+            console.log("complete");
+            phantomProcess.dispose();
+            completeCallback(config);
+        } else {
+            var inputPath = inputFilePaths.pop();
+
+            var page = phantomProcess.createPage();
+
+            page.run(inputPath, function (libPath, resolve, reject) {
+
+                function extractRegistry(requirejs) { // see Utils.extractRegistry
+                    var completeRegistryObject = JSON.parse(JSON.stringify(requirejs.s.contexts._.registry));
+                    var completeRegistry = Object.keys(completeRegistryObject);
+                    return completeRegistry.filter(function(moduleName) {
+                        return moduleName.indexOf("_@") !== 0;
+                    });
+                }
+
+                var script = document.createElement('script');
+                script.onload = function() {
+                    console.log("Script loaded and ready");
+
+                    var initialRegistry = extractRegistry(requirejs);
+
+                    requirejs([ libPath ], function(amd) {
+
+                        var currentRegistry = extractRegistry(requirejs);
+                        console.log(currentRegistry);
+
+                        if (amd) {
+                            resolve({ type: "AMD" });
+                        } else if (currentRegistry.length > initialRegistry.length) {
+                            console.log("Module is Strictly named AMD");
+                            var moduleName;
+                            currentRegistry.forEach(function(registryEntry) {
+                                if(initialRegistry.indexOf(registryEntry) === -1) {
+                                    moduleName = registryEntry;
+                                }
+                            });
+
+                            resolve({ type: "SNAMD", name: moduleName });
+                        } else {
+                            resolve({ type: "BG" });
+                        }
+                    });
+
+                };
+                script.src = "http://requirejs.org/docs/release/2.1.15/minified/require.js";
+                document.getElementsByTagName('head')[0].appendChild(script);
+
+            }).then(function(moduleInfo) {
+                console.log(moduleInfo);
+                page.dispose();
+
+                var configBasePath = path.dirname(mainFile);
+                var modulePath = path.relative(configBasePath, inputPath);
+
+                switch(moduleInfo.type) {
+                    case "AMD":
+                        var moduleName = path.basename(inputPath, ".js");
+                        config.paths[moduleName] = modulePath;
+                        break;
+                    case "SNAMD":
+                        config.paths[moduleInfo.name] = modulePath;
+                        break;
+                    case "BG":
+                        var moduleName = path.basename(inputPath, ".js");
+                        config.paths[moduleName] = modulePath;
+                        break;
+                }
+
+                processInputFileIteration(inputFilePaths, phantomProcess)
+            });
+        }
+
+    }
+
+}
+
+
+
+module.exports = {
+    buildConfig: buildConfig
+}
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,18 @@
+require("es6-shim");
+
 function clone(object) {
     return JSON.parse(JSON.stringify(object));
 }
 
+// Extract the 'registry' from a RequireJS instance, giving all defined, explicitly named modules.
+function extractRegistry(requirejs) {
+    var completeRegistry = Object.keys(clone(registry.s.contexts._.registry));
+    return completeRegistry.filter(function(moduleName) {
+        return !moduleName.startsWith("_@"); // module maps used by requirejs internally.
+    });
+}
 
 module.exports = {
-    clone: clone
+    clone: clone,
+    extractRegistry: extractRegistry
 };
