806c9f0fd00788301485c456fa2be9aca035381e
braima01 <adam.braimah@bbc.co.uk>
2014-12-17 14:45:27 +0000

    Allow directories to be used as input

--- require-config.js
+++ require-config.js
@@ -1,5 +1,8 @@
+require("es6-shim");
 var package = require("./package.json");
 var coa = require("coa");
+var fs = require("fs");
+var path = require("path");
 
 module.exports = coa.Cmd()
     .name("require-config")
@@ -54,6 +57,25 @@ module.exports = coa.Cmd()
         // output file default
         var output = options.output || "requirejs-configuration.js"
 
-        require("./src/main.js").main(options.mainFile, options.input, output, options.browser, options.basePath);
+        // detect folders in set of input files, extract all .js file paths from these folders and replace
+        var inputFiles = [];
+        options.input.forEach(function(inputParameter){
+            if (fs.lstatSync(inputParameter).isFile()) {
+                inputFiles.push(inputParameter);
+            }
+            else {
+                // list all .js files within this folder and push them to inputFiles
+                var allFiles = fs.readdirSync(inputParameter);
+                allFiles.forEach(function(listingEntry){
+                    var fullFilename = path.join(inputParameter,listingEntry) ;
+                    if(fullFilename.endsWith(".js") && fs.lstatSync(fullFilename).isFile()) {
+                        inputFiles.push(fullFilename); 
+                    }
+                })
+            }
+        });
+
+
+        require("./src/main.js").main(options.mainFile, inputFiles, output, options.browser, options.basePath);
     })
 .run();
